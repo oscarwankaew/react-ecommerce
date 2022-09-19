5c536d0aad9fa6c3de62e1ae7e1fa54a1f934b4d
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-09-19 14:20:14 -0500

    installed jquery, @popperjs and bootstrap packages

---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "@popperjs/core/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+
+var element = <div>Hello World</div>;
+ReactDOM.render(element, document.getElementById("root"));
