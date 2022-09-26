08d1edc4783da267372f7748ec4ff0b83aed1ed4
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-09-26 18:40:25 -0500

    renamed MainContent.jsx to CustomerList.jsx amd created another file named ShoppingCart.jsx and rendering on App.jsx

--- my-app/src/CustomerList.jsx
+++ my-app/src/CustomerList.jsx
@@ -1,5 +1,5 @@
 import React, { Component} from "react";
-export default class MainContent extends Component{
+export default class CustomerList extends Component{
 
   state = {
     pageTitle: "Customers", 
