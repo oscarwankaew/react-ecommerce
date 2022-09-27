150fe92ad6d244f84e85d1be3dced781892b9379
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-09-26 19:06:28 -0500

    created Products.jsx file

--- my-app/src/NavBar.jsx
+++ my-app/src/NavBar.jsx
@@ -5,7 +5,7 @@ class NavBar extends Component{
     return(
       <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-stlye">
-          <a className="navbar-brand" href="/#">My App</a>
+          <a className="navbar-brand" href="/#">eCommerce</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
           </button>
--- my-app/src/ShoppingCart.jsx
+++ my-app/src/ShoppingCart.jsx
@@ -1,2 +1,22 @@
 import React, { Component } from "react";
-export default class ShoppingCart extends Component {}
+export default class ShoppingCart extends Component {
+
+  state = { 
+    products: [ 
+    {id: 1, productName: "iPhone 14", price: 1500, quantity: 0}, 
+    {id: 2, productName: "LG TV", price: 4500, quantity: 0},
+    {id: 3, productName: "Samsung TV", price: 500, quantity: 0},
+    {id: 4, productName: "MacBook Pro", price: 2000, quantity: 0},
+    {id: 5, productName: "PS5", price: 3000, quantity: 0},
+    {id: 6, productName: "Xbox 1", price: 3000, quantity: 0},
+  ],
+  };
+  
+  render() {
+    return (
+      <div>
+        <h4>Shopping Cart</h4>
+      </div>
+    );
+  }
+}
