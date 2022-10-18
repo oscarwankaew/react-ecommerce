82bab382c9d1c4df3394faca4b020b58e96ada3f
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-10-17 22:02:42 -0500

    implemented contructor class in ShoppingCart to initialize the state of objects

--- my-app/src/ShoppingCart.jsx
+++ my-app/src/ShoppingCart.jsx
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import Products from "./Products";
 export default class ShoppingCart extends Component {
 
-  state = { 
-    products: [ 
-    {id: 1, productName: "iPhone 14", price: 1500, quantity: 0}, 
-    {id: 2, productName: "LG TV", price: 4500, quantity: 0},
-    {id: 3, productName: "Samsung TV", price: 500, quantity: 0},
-    {id: 4, productName: "MacBook Pro", price: 2000, quantity: 0},
-    {id: 5, productName: "PS5", price: 3000, quantity: 0},
-    {id: 6, productName: "Xbox 1", price: 3000, quantity: 0},
-  ],
-  };
+  constructor() {
+    super();
+    this.state = { 
+      products: [ 
+        {id: 1, productName: "iPhone 14", price: 1500, quantity: 0}, 
+        {id: 2, productName: "LG TV", price: 4500, quantity: 0},
+        {id: 3, productName: "Samsung TV", price: 500, quantity: 0},
+        {id: 4, productName: "MacBook Pro", price: 2000, quantity: 0},
+        {id: 5, productName: "PS5", price: 3000, quantity: 0},
+        {id: 6, productName: "Xbox 1", price: 3000, quantity: 0},
+      ],
+    };
+  }
+
+  
   
   render() {
     return (
@@ -71,10 +76,13 @@ export default class ShoppingCart extends Component {
     let allProducts = [...this.state.products];
     let index = allProducts.indexOf(product);
 
-    // delete product based on index
-    allProducts.splice(index, 1);
+    if (window.confirm("Are you sure?")) {
+      // delete product based on index
+      allProducts.splice(index, 1);
 
-    // update the state of current component
-    this.setState({products: allProducts});
+      // update the state of current component
+      this.setState({products: allProducts});
+    }
+    
   }
 }
