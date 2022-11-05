f9acf2e912c39ddae3994c83abc83900157d647f
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-11-05 12:55:31 -0500

    created routes for each pages on App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomerList from "./CustomerList";
+import { Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 export default class App extends Component{
   render(){
     return (
-      <React.Fragment>
+      <BrowserRouter>
         <NavBar />
-        <Dashboard></Dashboard>
-      </React.Fragment>
+        <Route path="/" exact component={Login} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/customers" exact component={CustomerList} />
+        <Route path="/cart" exact component={ShoppingCart} />
+      </BrowserRouter>
     );
   } 
 }
